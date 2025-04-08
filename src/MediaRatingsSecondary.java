4191f8dd2f4d5145cc4c779fa1b76487d2f48424
Wes-Waterstreet <waterstreet.2@buckeyemail.osu.edu>
2025-04-08 17:55:46 -0400

    Added common methods and removed contracts

--- src/MediaRatingsSecondary.java
+++ src/MediaRatingsSecondary.java
@@ -1,42 +1,17 @@
 /**
- * Layered implementations of secondary methods for {@code Map}.
- *
- * @param <String>
- *            String entries for media
- * @param <Integer>
- *            Integer values for rating
+ * Layered implementations of secondary methods for {@code MediaRatings}.
  */
-@SuppressWarnings("hiding")
-public abstract class MediaRatingsSecondary<String, Integer>
-        implements MediaRatings<String, Integer> {
-    /**
-     * updates the old rating of the given media with the new rating.
-     *
-     * @param media
-     *            the media to be changed
-     * @param rating
-     *            The new rating
-     * @updates this
-     *
-     */
+public abstract class MediaRatingsSecondary implements MediaRatings {
+
     @Override
     public void updateRating(String media, int rating) {
         this.remove(media);
         this.add(media, rating);
     }
 
-    /**
-     * Finds the media with the given rating value and stores it in a new
-     * tracker.
-     *
-     * @param rating
-     *            the associated rating to be added
-     * @return new Media Ratings with only media of the given rating.
-     *
-     */
     @Override
-    public MediaRatings<String, Integer> allRatings(int rating) {
-        MediaRatings<String, Integer> result = this.newInstance();
+    public MediaRatings allRatings(int rating) {
+        MediaRatings result = this.newInstance();
         for (int i = 0; i < this.numberOfRatings(); i++) {
             MediaRatings.Pair<String, Integer> temp = this.removeAny();
             if (temp.rating() == rating) {
@@ -45,5 +20,63 @@ public abstract class MediaRatingsSecondary<String, Integer>
             this.add(temp.media(), temp.rating());
         }
         return result;
+    }
+
+    @Override
+    public String toString() {
+        String value = "";
+        int length = this.length();
+        for (int i = 0; i < length; i++) {
+            MediaRatings temp = this.removeAny();
+            value.concat("<");
+            value.concat(temp.media.toString());
+            value.concat(", ");
+            value.concat(temp.rating.toString());
+            value.concat(">");
+            value.concat(", ")
+            this.add(temp);
+        }
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        int length = this.length();
+        for (int i = 0; i < length; i++) {
+            MediaRatings temp = this.removeAny();
+            result += (temp.media.hashCode());
+            this.add(temp);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        MediaRatings<T> obj = (MediaRatings<T>) o;
+
+        if (this.length() != obj.length()) {
+            return false;
+        }
+
+        int length = this.length();
+        for (int i = 0; i < length; i++) {
+            MediaRatings thisItem = this.remove();
+            MediaRatings otherItem = obj.remove();
+            if (!thisItem.equals(otherItem)) {
+                return false;
+            }
+            this.add(thisItem);
+            obj.add(otherItem);
+        }
+        return true;
     }
 }
