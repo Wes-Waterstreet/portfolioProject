a073d22afe49344ee278948b10b4cefb27cc9dcf
Wes-Waterstreet <waterstreet.2@buckeyemail.osu.edu>
2025-04-08 20:36:38 -0400

    Revert "fixed secondary methods"
    This reverts commit 1ed32e05e7a431910f5ece8c03d136c93dd14843.

--- src/MediaRatingsSecondary.java
+++ src/MediaRatingsSecondary.java
@@ -4,16 +4,16 @@
 public abstract class MediaRatingsSecondary implements MediaRatings {
 
     @Override
-    public final void updateRating(String media, int rating) {
+    public void updateRating(String media, int rating) {
         this.remove(media);
         this.add(media, rating);
     }
 
     @Override
-    public final MediaRatings allRatings(int rating) {
+    public MediaRatings allRatings(int rating) {
         MediaRatings result = this.newInstance();
         for (int i = 0; i < this.numberOfRatings(); i++) {
-            MediaRatings.Pair temp = this.removeAny();
+            MediaRatings.Pair<String, Integer> temp = this.removeAny();
             if (temp.rating() == rating) {
                 result.add(temp.media(), rating);
             }
@@ -23,64 +23,59 @@ public abstract class MediaRatingsSecondary implements MediaRatings {
     }
 
     @Override
-    public final String toString() {
+    public String toString() {
         String value = "";
-        int length = this.numberOfRatings();
+        int length = this.length();
         for (int i = 0; i < length; i++) {
-            MediaRatings.Pair temp = this.removeAny();
+            MediaRatings temp = this.removeAny();
             value.concat("<");
-            value.concat(temp.media().toString());
+            value.concat(temp.media.toString());
             value.concat(", ");
-            value.concat(String.valueOf(temp.rating()));
+            value.concat(temp.rating.toString());
             value.concat(">");
-            value.concat(", ");
-            this.add(temp.media(), temp.rating());
+            value.concat(", ")
+            this.add(temp);
         }
         return value;
     }
 
     @Override
-    public final int hashCode() {
+    public int hashCode() {
         int result = 1;
-        int length = this.numberOfRatings();
+        int length = this.length();
         for (int i = 0; i < length; i++) {
-            MediaRatings.Pair temp = this.removeAny();
-            result += (temp.media().hashCode());
-            this.add(temp.media(), temp.rating());
+            MediaRatings temp = this.removeAny();
+            result += (temp.media.hashCode());
+            this.add(temp);
         }
         return result;
     }
 
     @Override
-    public final boolean equals(Object o) {
+    public boolean equals(Object o) {
         if (this == o) {
             return true;
         }
 
-        if (o == null || this.getClass() != o.getClass()) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
 
-        MediaRatings obj = (MediaRatings) o;
+        MediaRatings<T> obj = (MediaRatings<T>) o;
 
-        if (this.numberOfRatings() != obj.numberOfRatings()) {
+        if (this.length() != obj.length()) {
             return false;
         }
 
-        int length = this.numberOfRatings();
-        MediaRatings temp = this.newInstance();
-        temp.transferFrom(this);
+        int length = this.length();
         for (int i = 0; i < length; i++) {
-            MediaRatings.Pair thisPair = temp.removeAny();
-            if (!obj.hasMedia(thisPair.media())) {
+            MediaRatings thisItem = this.remove();
+            MediaRatings otherItem = obj.remove();
+            if (!thisItem.equals(otherItem)) {
                 return false;
             }
-            MediaRatings.Pair objPair = obj.remove(thisPair.media());
-            if (!(objPair.rating() == (thisPair.rating()))) {
-                return false;
-            }
-            this.add(thisPair.media(), thisPair.rating());
-            obj.add(objPair.media(), objPair.rating());
+            this.add(thisItem);
+            obj.add(otherItem);
         }
         return true;
     }
