a64abb14077166a0b6c289fa9ae56ee98585cbf6
Wes-Waterstreet <waterstreet.2@buckeyemail.osu.edu>
2025-04-18 22:33:25 -0400

    created two use cases for MediaRatings

--- src/Movies.java
+++ src/Movies.java
@@ -1,5 +1,6 @@
-package components.mediaratings;
 
+import components.mediaratings.MediaRatings;
+import components.mediaratings.MediaRatings1L;
 import components.simplewriter.SimpleWriter;
 import components.simplewriter.SimpleWriter1L;
 
@@ -26,13 +27,14 @@ public final class Movies {
     public static void main(String[] args) {
         SimpleWriter out = new SimpleWriter1L();
         MediaRatings movies = new MediaRatings1L();
-        movies.add("movie 1", 3);
-        movies.add("movie 2", 4);
-        movies.add("movie 3", 2);
-        movies.add("movie 4", 5);
-        movies.add("movie 5", 4);
+        movies.add("movie1", 3);
+        movies.add("movie2", 4);
+        movies.add("movie3", 2);
+        movies.add("movie4", 5);
+        movies.add("movie5", 4);
         MediaRatings fourStars = movies.allRatings(4);
         int length = fourStars.numberOfRatings();
+        out.println(movies.toString());
         out.println("Four star movies: ");
         for (int i = 0; i < length; i++) {
             out.println(fourStars.removeAny().media());
